54560d8d3348e7f46c1f6bfb444e54d05ab9ea6b
Renan <orefusado@gmail.com>
2023-02-19 03:34:32 -0300

    generating all pieces

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const blocksNo = 9;
+const pieces = blocksNo;
 
-function getRandom(min = 1, max = blocksNo, except = []) {
+function randomNumber(min = 1, max = blocksNo, except = []) {
   let number, random;
 
   while (!number) {
@@ -11,5 +12,33 @@ function getRandom(min = 1, max = blocksNo, except = []) {
       number = random;
   }
 
-  return number.toString();
+  return number;
+}
+
+function randomSequence(index = 1, offset = blocksNo, length = blocksNo, sequence = []){
+  for (let i = 0; i < length; i++) {
+    const number = randomNumber(index, offset, sequence);
+    sequence.push(number);
+  }
+
+  return sequence;
+}
+
+let firstPieces;
+while (!firstPieces) {
+  const sequence = randomSequence();
+
+  let hasEquals = false;
+  for (let i = 0; i < sequence.length; i++) {
+    if (sequence[i] == i + 1) hasEquals = true;
+  }
+
+  if (!hasEquals) firstPieces = sequence;
+}
+
+const blocks = [];
+for (let i = 0; i < blocksNo; i++) {
+  const first = firstPieces[i];
+
+  blocks.push(randomSequence(1, blocksNo, blocksNo - 1, [first]));
 }

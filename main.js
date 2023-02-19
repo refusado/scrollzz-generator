6eae5cfdfa33459be5b6c96f29e54d2efb5204cc
Renan <orefusado@gmail.com>
2023-02-19 12:57:18 -0300

    generating the css file with the content

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-const blocksNo = 5;
-const piecesNo = blocksNo;
+const fs = require('fs');
+
+const level = 5;
+const resultPath = `./level-${level}.css`;
+
+const data = {
+  0: { block: 4, image: "star" },
+  1: { block: 8, image: "polygon" },
+  2: { block: 9, image: "circle" },
+  3: { block: 16, image: "cube" },
+  4: { block: 16, image: "moon" },
+  5: { block: 20, image: "triangle" }  
+}
+const blocksNo = data[level].block;
+const image = data[level].image;
 
 function randomNumber(min = 1, max = blocksNo, except = []) {
   let number, random;
@@ -44,13 +57,37 @@ for (let i = 0; i < blocksNo; i++) {
 }
 
 const piecesPositions = [];
-for (let i = 1; i <= piecesNo; i++) {
+for (let i = 0; i < blocksNo; i++) {
   const currPiece = [];
+  const piece = i + 1;
 
   for (let j = 0; j < blocks.length; j++) {
-    const position = blocks[j].indexOf(i) + 1;
+    const position = blocks[j].indexOf(piece) + 1;
     currPiece.push(position);
   }
 
   piecesPositions.push(currPiece);
 }
+
+let fileContent = `/*
+  Automatically generated file
+  Generator: https://github.com/refusado/scrollzz-generator
+*/ \n\n`;
+
+piecesPositions.forEach((currPiece, pieceIndex) => {
+
+  currPiece.forEach((position, positionIndex) => {
+    fileContent += `#b${level}${positionIndex + 1} .tile:nth-child(${position})`;
+
+    if (currPiece.length - 1 == positionIndex) {
+      fileContent += ` {\n  background-image: var(--${image}-${pieceIndex + 1});\n}\n\n`;
+    } else {
+      fileContent += `,\n`;
+    }
+  });
+
+});
+
+fs.writeFile(resultPath, fileContent, function (err) {
+  if (err) throw err;
+});
